4b79c3a28f08c50a400065dd0861b373f8ce10ab
bs0902 <bscontreras09@misena.edu.co>
2023-10-22 13:25:21 -0500

    Prueba

---
+++ compra.py
@@ -0,0 +1,23 @@
+añosempresa = int (input("digite años"))
+if (añosempresa>=5 and añosempresa <=10):
+    print ("principiante")
+elif (añosempresa>=11 and añosempresa <=20):
+    print ("formacion")
+elif (añosempresa>=21 and añosempresa <=40):
+    print ("junior")
+elif (añosempresa>=41 and añosempresa <=50):
+    print ("novato")
+elif (añosempresa>=51 and añosempresa <=60):
+    print ("experto")
+añosdocente = int (input("digite años docente"))
+if (añosdocente)>=0:
+    print ("principiante")
+elif (añosdocente)>=5:
+    print ("formcion")
+elif (añosdocente)>=10:
+    print ("junior")
+elif (añosdocente)>=20:
+    print ("novato")
+elif (añosdocente)>=30:
+    print ("experto")
+edad = int(input("digite edad"))
